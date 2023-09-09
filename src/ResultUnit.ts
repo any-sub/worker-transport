4875535e7b1dfeb257fe2ccf1a02b019230761a9
Stefan <stefan@stefanpuia.co.uk>
2023-09-09 18:27:24 +0100

    field size restrictions

--- src/ResultUnit.ts
+++ src/ResultUnit.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const ResultUnitParser = z.object({
-  title: z.string().nullish(),
-  description: z.string().nullish(),
-  url: z.string().url().nullish(),
-  image: z.string().url().nullish(),
+  title: z.string().max(250).nullish(),
+  description: z.string().max(500).nullish(),
+  url: z.string().url().max(2048).nullish(),
+  image: z.string().url().max(2048).nullish(),
   hash: z.string().describe("Hashed version of the contents")
 });
 
